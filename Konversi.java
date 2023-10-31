9f3d152d9db393cffdfc6c96b256dc193a1c0b42
Marufpr <sultanprabowo90@gmail.com>
2023-10-31 11:28:37 +0700

    pesan commit

---
+++ Konversi.java
@@ -0,0 +1,8 @@
+public class Koversi {
+    public static void main(String[] args) {
+        double meter = 5;
+        double centimeter = meter * 100;
+
+        System.out.println(meter + " meter sama dengan " + centimeter + " sentimeter.");
+    }
+}
